327ef969eb3c9a1a8082d494801d658114d1cbaf
ashutosh <ashutoshsahni833@gmail.com>
2025-05-19 00:58:44 +0530

    m

--- main.java
+++ main.java
@@ -1,4 +1,14 @@
 
-public static void main(String[] args) {
-    const le
+public class Main{
+    public static void main(String[] args) {
+        //roll number no 5 students //
+        int[] number = new int[5];
+
+        //store  5 names //
+         String [] names = new String[5]; 
+
+    
+        
+    }
+        
 }
